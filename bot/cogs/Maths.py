38dac559c41391a73b0575074426bf46589cef2c
Isaac Taylor <isaacjontaylor@gmail.com>
2021-03-05 09:01:30 +0000

    Moved to custom parser

--- bot/cogs/Maths.py
+++ bot/cogs/Maths.py
@@ -1,150 +1,99 @@
 from __future__ import division
-from pyparsing import (Literal, CaselessLiteral, Word, Combine, Group, Optional,
-                       ZeroOrMore, Forward, nums, alphas, oneOf)
+
 import math
 import operator
 
 import discord
+import pyparsing
 from discord.ext import commands
+from pyparsing import (CaselessLiteral, Combine, Forward, Group, Literal,
+                       Optional, Word, ZeroOrMore, alphas, nums, oneOf)
+
+from .backend.maths.main import solve
+
 
 def cmp(a, b):
     return (a > b) - (a < b)
 
-class NumericStringParser(object):
-    def pushFirst(self, strg, loc, toks):
-        self.exprStack.append(toks[0])
-
-    def pushUMinus(self, strg, loc, toks):
-        if toks and toks[0] == '-':
-            self.exprStack.append('unary -')
-
-    def __init__(self):
-        """
-        expop   :: '^'
-        multop  :: '*' | '/'
-        addop   :: '+' | '-'
-        integer :: ['+' | '-'] '0'..'9'+
-        atom    :: PI | E | real | fn '(' expr ')' | '(' expr ')'
-        factor  :: atom [ expop factor ]*
-        term    :: factor [ multop factor ]*
-        expr    :: term [ addop term ]*
-        """
-        point = Literal(".")
-        e = CaselessLiteral("E")
-        fnumber = Combine(Word("+-" + nums, nums) +
-                          Optional(point + Optional(Word(nums))) +
-                          Optional(e + Word("+-" + nums, nums)))
-        ident = Word(alphas, alphas + nums + "_$")
-        plus = Literal("+")
-        minus = Literal("-")
-        mult = Literal("*")
-        div = Literal("/")
-        lpar = Literal("(").suppress()
-        rpar = Literal(")").suppress()
-        addop = plus | minus
-        multop = mult | div
-        expop = Literal("^")
-        pi = CaselessLiteral("PI")
-        expr = Forward()
-        atom = ((Optional(oneOf("- +")) +
-                 (ident + lpar + expr + rpar | pi | e | fnumber).setParseAction(self.pushFirst))
-                | Optional(oneOf("- +")) + Group(lpar + expr + rpar)
-                ).setParseAction(self.pushUMinus)
-        # by defining exponentiation as "atom [ ^ factor ]..." instead of
-        # "atom [ ^ atom ]...", we get right-to-left exponents, instead of left-to-right
-        # that is, 2^3^2 = 2^(3^2), not (2^3)^2.
-        factor = Forward()
-        factor << atom + \
-            ZeroOrMore((expop + factor).setParseAction(self.pushFirst))
-        term = factor + \
-            ZeroOrMore((multop + factor).setParseAction(self.pushFirst))
-        expr << term + \
-            ZeroOrMore((addop + term).setParseAction(self.pushFirst))
-        # addop_term = ( addop + term ).setParseAction( self.pushFirst )
-        # general_term = term + ZeroOrMore( addop_term ) | OneOrMore( addop_term)
-        # expr <<  general_term
-        self.bnf = expr
-        # map operator symbols to corresponding arithmetic operations
-        epsilon = 1e-12
-        self.opn = {"+": operator.add,
-                    "-": operator.sub,
-                    "*": operator.mul,
-                    "/": operator.truediv,
-                    "^": operator.pow}
-        self.fn = {"sin": math.sin,
-                   "cos": math.cos,
-                   "tan": math.tan,
-                   "exp": math.exp,
-                   "abs": abs,
-                   "trunc": lambda a: int(a),
-                   "round": round,
-                   "sgn": lambda a: abs(a) > epsilon and cmp(a, 0) or 0}
-
-    def evaluateStack(self, s):
-        op = s.pop()
-        if op == 'unary -':
-            return -self.evaluateStack(s)
-        if op in "+-*/^":
-            op2 = self.evaluateStack(s)
-            op1 = self.evaluateStack(s)
-            return self.opn[op](op1, op2)
-        elif op == "PI":
-            return math.pi  # 3.1415926535
-        elif op == "E":
-            return math.e  # 2.718281828
-        elif op in self.fn:
-            return self.fn[op](self.evaluateStack(s))
-        elif op[0].isalpha():
-            return 0
-        else:
-            return float(op)
-
-    def eval(self, num_string, parseAll=True):
-        self.exprStack = []
-        _results = self.bnf.parseString(num_string, parseAll)
-        val = self.evaluateStack(self.exprStack[:])
-        return val
 
 class Maths(commands.Cog):
     '''Can't be bothered to do your maths homework? I'll try help!'''
+
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name = "solve", aliases=["math", "calculate", "calc"])
+    async def splitsolve(self, expression, splitby):
+        things = expression.split(splitby, 1)
+
+        for num, i in enumerate(things):
+            things[num] = await self.solve(i)
+
+        if (type(things[num]) in [int, float]) and type(things[num]) == float and things[num].is_integer():
+            things[num] = int(things[num])
+
+        return all(e == things[0] for e in things)
+
+    async def solve(self, expression):
+        altered = expression.replace("**", "^")
+        altered = altered.replace("\U000000f7", "/")
+
+        # if betterbool(expression) == True:
+        #     return "True"
+
+        # if altered.startswith("return "):
+        #     return await self.solve(altered[7:len(altered)])
+
+        # if "=" in altered and not "==" in altered:
+        #     return await self.splitsolve(altered, "=")
+
+        # elif "==" in altered:
+        #     return await self.splitsolve(altered, "==")
+
+        # elif "and" in altered.lower():
+        #     return await self.splitsolve(altered, "and")
+
+        answer = await solve(expression)
+
+        if type(answer) in [float] and answer.is_integer():
+            answer = int(answer)
+
+        return answer
+
+    @commands.command(name="solve", aliases=["math", "calculate", "calc", "maths"])
     async def _solve(self, ctx, *, expression):
         '''
         Solve a maths equation
         '''
-        nsp = NumericStringParser()
-        try:
-            if "=" in expression:
-                things = expression.split("=")
-                
-                for num, i in enumerate(things):
-                    things[num] = nsp.eval(i)
 
-                if things[num].is_integer():
-                    things[num] = int(things[num])
-                
-                answer = all(e == things[0] for e in things)
-            
-            else:
-                answer = nsp.eval(expression)
-                
-                if answer.is_integer():
-                    answer = int(answer)
-                
-            embed = discord.Embed(
-                title="Solved!", colour=discord.Colour.green())
+        try:
+            answer = await self.solve(expression)
+
+            embed = discord.Embed(colour=discord.Colour.green())
             embed.add_field(name="Input", value=f"```\n{expression}```")
             embed.add_field(name="Output", value=f"```\n{answer}```")
         except Exception as e:
-            embed = discord.Embed(title="Error!", colour=discord.Colour.red())
-            embed.add_field(name="Input", value=f"```\n{expression}```")
-            embed.add_field(name="Error", value=f"```\n{e}```")
+            if isinstance(e, pyparsing.ParseException):
+                embed = discord.Embed(
+                    title="Error!", colour=discord.Colour.red())
+                embed.add_field(name="Input", value=f"```\n{expression}```")
+                embed.add_field(
+                    name="Error", value=f"```\nUnexpected input \"{e.line}\"```")
+            elif isinstance(e, OverflowError):
+                embed = discord.Embed(
+                    title="Error!", colour=discord.Colour.red())
+                embed.add_field(name="Input", value=f"```\n{expression}```")
+                embed.add_field(
+                    name="Error", value=f"```\nOverflowError```")
+            else:
+                # raise
+                embed = discord.Embed(
+                    title="Error!", colour=discord.Colour.red())
+                embed.add_field(name="Input", value=f"```\n{expression}```")
+                embed.add_field(
+                    name="Error", value=f"```\n{str(e)}```")
         finally:
-            await ctx.send(embed=embed)
-            
+            await ctx.reply(embed=embed, mention_author=False)
+
 
 def setup(bot):
     bot.add_cog(Maths(bot))
