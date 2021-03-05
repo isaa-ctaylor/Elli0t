f611e098dd84ef98ae64a28f77affb2951e5939c
Isaac Taylor <isaacjontaylor@gmail.com>
2021-03-05 08:58:46 +0000

    Added xkcd

--- bot/cogs/Fun.py
+++ bot/cogs/Fun.py
@@ -1,11 +1,14 @@
 import discord
 import random
 from discord.ext import commands
+import xkcd_wrapper
+import aiohttp
 
 
 class Fun(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.xkcd = xkcd_wrapper.AsyncClient()
 
     @commands.command(name="8ball")
     async def _8ball(self, ctx, *, question):
@@ -63,6 +66,26 @@ class Fun(commands.Cog):
         ]
         
         await ctx.send(f"{ctx.author.mention} licked {person.mention}! They taste like {random.choice(tastesLike)}!")
+        
+    @commands.command(name="xkcd")
+    async def _xkcd(self, ctx, number: int=None):
+        try:
+            with ctx.typing():
+                if number:
+                    try:
+                        comic = await self.xkcd.get(number)
+                    except TypeError:
+                        embed = discord.Embed(title="Error!", description=f"Couldn't find comic {number}", colour=discord.Colour.red())
+                else:
+                    comic = await self.xkcd.get_random()
+                
+                if comic:
+                    embed = discord.Embed(title=comic.title, description=f"[Comic number {comic.id}]({comic.comic_url})\n{comic.description}", colour=discord.Colour.random())
+                    embed.set_image(url = comic.image_url)
+        
+        except:
+            embed = discord.Embed(title="Error!", description="There was an error, please try again soon", colour=discord.Colour.red())        
+        await ctx.send(embed = embed)
         
 def setup(bot):
     bot.add_cog(Fun(bot))
