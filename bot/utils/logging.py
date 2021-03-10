eeee3c9b84473ebfcb0443c2b2675caeab0088bd
Isaac Taylor <isaacjontaylor@gmail.com>
2021-03-10 18:20:01 +0000

    Added time-based filenames

--- bot/utils/logging.py
+++ bot/utils/logging.py
@@ -1,10 +1,22 @@
 import logging
+import datetime
+import os
 
-def logger_setup():
+dirname = os.path.dirname(__file__)
+
+
+def logger_setup(bot):
     logger = logging.getLogger('discord')
     logger.setLevel(logging.INFO)
+    time: datetime.datetime = datetime.datetime.now()
+    time = time.strftime(r"%m%d%Y %H%M%S")
+    filename = os.path.join(dirname, f'../logs/{time}.log')
+    print(filename)
+    open(filename, "x").close()
     handler = logging.FileHandler(
-        filename='/media/Elli0t/Elli0t/bot/logs/latest.log', encoding='utf-8', mode='w')
+        filename=filename, encoding='utf-8', mode='w')
     handler.setFormatter(logging.Formatter(
         '%(filename)s - %(asctime)s - %(levelname)s - %(message)s'))
     logger.addHandler(handler)
+    bot.loggingfilename = filename
+    return bot
