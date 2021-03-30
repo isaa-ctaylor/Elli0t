6c332a3f075e102bc16d89aa3f01ef1d714ffe7f
Sitanshu15 <80409424+Sitanshu-15@users.noreply.github.com>
2021-03-30 10:38:12 +0545

    Added RPS command :|

--- bot/cogs/Games.py
+++ bot/cogs/Games.py
@@ -414,6 +414,44 @@ class Games(commands.Cog):
                             my_list) if x == message.content.lower()]
                         
 #                        for i in 
+
+    @commands.command(aliases=['rock paper sissors'])
+    async def rps(self, ctx):
+        '''Well pretty self explanatory a rock paper Scissors commands'''
+        """Usage :  }rps or }rock paper sissors """#No parms
+        rpsGame = ['rock', 'paper', 'scissors']
+        e = discord.Embed(title=f"{ctx.author.name}Lets start", description=f"Choose one Rock , Paper , Scissors ")#Just a started  message
+        await ctx.send(embed=e)#
+
+        def check(msg):#no async :>
+            return msg.author == ctx.author and msg.channel == ctx.channel and msg.content.lower() in rpsGame
+
+        user_choice = (await self.bot.wait_for('message', check=check)).content
+
+        comp_choice = random.choice(rpsGame)
+        if user_choice == 'rock':
+            if comp_choice == 'rock':
+                await ctx.send(f'Well, that was weird. We tied.\nYour choice: {user_choice}\nMy choice: {comp_choice}')
+            elif comp_choice == 'paper':
+                await ctx.send(f'Nice try, but I won that time!!\nYour choice: {user_choice}\nMy choice: {comp_choice}')
+            elif comp_choice == 'scissors':
+                await ctx.send(f"Aw, you beat me. It won't happen again!\nYour choice: {user_choice}\nMy choice: {comp_choice}")
+
+        elif user_choice == 'paper':
+            if comp_choice == 'rock':
+                await ctx.send(f'The pen beats the sword? More like the paper beats the rock!!\nYour choice: {user_choice}\nMy choice: {comp_choice}')
+            elif comp_choice == 'paper':
+                await ctx.send(f'Oh, wacky. We just tied. I call a rematch!!\nYour choice: {user_choice}\nMy choice: {comp_choice}')
+            elif comp_choice == 'scissors':
+                await ctx.send(f"Aw man, you actually managed to beat me.\nYour choice: {user_choice}\nMy choice: {comp_choice}")
+
+        elif user_choice == 'scissors':
+            if comp_choice == 'rock':
+                await ctx.send(f'HAHA!! I JUST CRUSHED YOU!! I rock!!\nYour choice: {user_choice}\nMy choice: {comp_choice}')
+            elif comp_choice == 'paper':
+                await ctx.send(f'Bruh. >: |\nYour choice: {user_choice}\nMy choice: {comp_choice}')
+            elif comp_choice == 'scissors':
+                await ctx.send(f"Oh well, we tied.\nYour choice: {user_choice}\nMy choice: {comp_choice}")
                     
 
 def setup(bot):
