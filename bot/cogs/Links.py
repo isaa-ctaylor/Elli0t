3c4165f47b30b9ad46796885d0dd5ee39aa4dd0b
Isaac Taylor <64303279+isaa-ctaylor@users.noreply.github.com>
2021-05-08 08:33:30 +0100

    Changed await ctx.send to await ctx.reply

--- bot/cogs/Links.py
+++ bot/cogs/Links.py
@@ -30,43 +30,49 @@ class Links(commands.Cog):
     '''
     A couple helpful links
     '''
+
     def __init__(self, bot):
-        self.bot=bot
+        self.bot = bot
 
-
-    @commands.command(name = "github", aliases = ["source", "git", "code"])
+    @commands.command(name="github", aliases=["source", "git", "code"])
     async def _github(self, ctx):
         '''
         Find the source code on github
-        ''' 
-        github_embed = discord.Embed(title = "Github", description = f"Find the source code [here](https://www.github.com/isaa-ctaylor/Elli0t/)")
-        github_embed.set_thumbnail(url = "https://avatars1.githubusercontent.com/u/9919")
-        await ctx.send(embed = github_embed)
-    
-    @commands.command(name = "ko-fi", aliases = ["coffee", "donate"])
+        '''
+        github_embed = discord.Embed(
+            title="Github", description=f"Find the source code [here](https://www.github.com/isaa-ctaylor/Elli0t/)")
+        github_embed.set_thumbnail(
+            url="https://avatars1.githubusercontent.com/u/9919")
+        await ctx.reply(embed=github_embed, mention_author=False)
+
+    @commands.command(name="ko-fi", aliases=["coffee", "donate"])
     async def _ko_fi(self, ctx):
         '''
         Buy me a coffee
         '''
-        kofi_embed = discord.Embed(title = "Buy me a coffee", description = f"Click [here](https://ko-fi.com/isaa_ctaylor) to buy me a coffee", colour = self.bot.neutral_embed_colour)
-        kofi_embed.set_thumbnail(url = "https://storage.ko-fi.com/cdn/useruploads/90f7e47d-7c60-4338-b544-dbf2e6196dca.png")
-        await ctx.send(embed = kofi_embed)
-    
-    @commands.command(name = "support", aliases = ["helpserver"])
+        kofi_embed = discord.Embed(
+            title="Buy me a coffee", description=f"Click [here](https://ko-fi.com/isaa_ctaylor) to buy me a coffee", colour=self.bot.neutral_embed_colour)
+        kofi_embed.set_thumbnail(
+            url="https://storage.ko-fi.com/cdn/useruploads/90f7e47d-7c60-4338-b544-dbf2e6196dca.png")
+        await ctx.reply(embed=kofi_embed, mention_author=False)
+
+    @commands.command(name="support", aliases=["helpserver"])
     async def _support(self, ctx):
         '''
         Get a link to the support server
         '''
-        support_embed = discord.Embed(title = "Support server", description = "Click [here](https://discord.gg/QFmJTNTAry) to join the support server")
-        support_embed.set_thumbnail(url = self.bot.user.avatar_url)
-        await ctx.send(embed = support_embed)
-        
-    @commands.command(name = "invite")
+        support_embed = discord.Embed(
+            title="Support server", description="Click [here](https://discord.gg/QFmJTNTAry) to join the support server")
+        support_embed.set_thumbnail(url=self.bot.user.avatar.url)
+        await ctx.reply(embed=support_embed, mention_author=False)
+
+    @commands.command(name="invite")
     async def _invite(self, ctx):
         '''Invite the bot to your server!'''
         embed = discord.Embed(
-            title="Invite me!", description="Click [here](https://discord.com/api/oauth2/authorize?client_id=778637164388810762&permissions=8&scope=bot) to add me to your server!")
+            title="Invite me!", description="Click [here](https://isaa-ctaylor.github.io/Elli0t/invite/) to add me to your server!")
         await ctx.reply(embed=embed, mention_author=False)
-        
+
+
 def setup(bot):
     bot.add_cog(Links(bot))
